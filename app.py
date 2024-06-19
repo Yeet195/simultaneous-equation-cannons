2d798a8523b655f745be4c5f284494ee0c1140a1
Yeet195 <nils.kalinke11@gmail.com>
2024-06-19 11:49:47 +0200

    Fix application crash issues

--- app.py
+++ app.py
@@ -49,4 +49,6 @@ def calculate():
         })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
